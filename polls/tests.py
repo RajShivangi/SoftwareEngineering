deb963aa06deae4960fd2002625d3e5cb03cfa47
Shivangi Raj <shivangiraj@macbookpro.mynetworksettings.com>
2025-10-19 16:43:02 -0400

    Adding installed package

--- polls/tests.py
+++ polls/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 class BasicTestCase(TestCase):
     def test_addition(self):
         """Basic sanity check to ensure tests run."""
+        print("Hello, World!")
         self.assertEqual(1 + 1, 2)
 
     def test_truth(self):
